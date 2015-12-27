5e220f0ab1066d53b9be8d94dcbf63736bebbc4a
agdt3 <pavel.a@aol.com>
2015-12-27 00:15:11 -0500

    set correct image to pixel remapping

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 	"image/color"
 	"image/draw"
 	"image/jpeg"
-	//"math"
+	"math"
 	"os"
 )
 
@@ -17,29 +17,49 @@ type Ray struct {
 	Direction vec.Vec3
 }
 
-type CameraPerspective struct {
-	Origin vec.Vec3
-	Dir    vec.Vec3
+type Camera struct {
+	Origin      vec.Vec3
+	Dir         vec.Vec3
+	Width       int
+	Height      int
+	FOVX        float64
+	FOVY        float64
+	AspectRatio float64
+	Angle       float64
 }
 
-type CameraTHEOTHERONE struct {
-	Origin vec.Vec3
-	Dir    vec.Vec3
+func MakePerspectiveCamera(org, dir vec.Vec3, w, h int, fovx, fovy float64) *Camera {
+	cam := new(Camera)
+	cam.Origin = org
+	cam.Dir = dir
+	cam.Width = w
+	cam.Height = h
+	cam.FOVX = fovx
+	cam.FOVY = fovy
+	cam.AspectRatio = float64(w) / float64(h)
+	cam.Angle = math.Tan((fovx * 0.5) / 57.296) // convert degree to radians
+
+	return cam
+}
+
+func (c Camera) ConvertPosToPixel(x, y int) (float64, float64) {
+	px := (2.0*((float64(x)+0.5)/float64(c.Width)) - 1.0) * c.Angle * c.AspectRatio
+	py := (1.0 - 2.0*((float64(y)+0.5)/float64(c.Height))) * c.Angle
+	return px, py
 }
 
 type RayTraceConfig struct {
 	UseLight       bool
 	UseShadows     bool
 	MaxReflections uint
-	ImageWidth     int
-	ImageHeight    int
 }
 
 type World struct {
-	Cam CameraPerspective
+	Cam *Camera
 	Img draw.Image // use the draw interface
 	//    Obj []Objects
 	Config RayTraceConfig
+	sphere *Sphere
 }
 
 func NewWorld() *World {
@@ -48,23 +68,32 @@ func NewWorld() *World {
 	dir := vec.MakeVec3(0, 0, -1)
 	org := vec.MakeVec3(0, 0, 0)
 
-	world.Cam = CameraPerspective{*org, *dir}
-	world.Config = RayTraceConfig{true, true, 1, 640, 480}
-	world.Img = image.NewRGBA(image.Rect(0, 0, world.Config.ImageWidth, world.Config.ImageHeight))
+	center := vec.MakeVec3(0, 0, -4)
+	sphere := Sphere{*center, 1, color.RGBA{255, 255, 255, 1}, 1}
+
+	world.Cam = MakePerspectiveCamera(*org, *dir, 640, 480, 45, 45)
+	//world.Cam = CameraPerspective{*org, *dir}
+	world.Config = RayTraceConfig{true, true, 1}
+	world.Img = image.NewRGBA(image.Rect(0, 0, world.Cam.Width, world.Cam.Height))
+	world.sphere = &sphere
 	//    world.Obj = nil // TODO: vec.Make objects
 
 	return world
 }
 
-func (w World) traceRay(origin vec.Vec3, dir vec.Vec3) color.RGBA {
-	return color.RGBA{0, 0, 255, 0}
+func (w World) traceRay(ray Ray) color.RGBA {
+	//isHit, hit, n, t0, t1 := w.sphere.Intersects(ray)
+	isHit, _, _, _, _ := w.sphere.Intersects(ray)
+	if isHit {
+		return w.sphere.Col
+	}
+	return color.RGBA{0, 0, 0, 0}
 }
 
-func (w World) generateRay(x, y, z float64) *vec.Vec3 {
+func (w World) MakeRay() {
 	// generates a vector3 that is assumed
 	// have an origin at (0,0,0)
-	vec3 := vec.MakeVec3(x, y, z)
-	return vec3
+	//return Ray{Vec3{}, Vec3{}}
 }
 
 func (w *World) Trace() {
@@ -72,11 +101,13 @@ func (w *World) Trace() {
 	fmt.Println(b)
 	for y := b.Min.Y; y < b.Max.Y; y++ {
 		for x := b.Min.X; x < b.Max.X; x++ {
-			dir := vec.MakeVec3(0, 0, -1)
+			px, py := w.Cam.ConvertPosToPixel(x, y)
 			origin := vec.MakeVec3(0, 0, 0)
-			pixelColor := w.traceRay(*origin, *dir)
-			w.Img.Set(x, y, pixelColor)
-			//            fmt.Println(w.Img.At(x,y))
+			dir := vec.MakeVec3(px, py, -1)
+			dir.Normalize()
+			ray := Ray{"camera", *origin, *dir}
+			pixelColor := w.traceRay(ray)
+			w.Img.Set(x, w.Cam.Height-y, pixelColor)
 		}
 	}
 
@@ -91,12 +122,14 @@ func (w *World) Trace() {
 
 type Object interface {
 	Intersects(Ray) (bool, vec.Vec3, vec.Vec3, float64, float64)
+	GetColor() color.Color
 }
 
 type Sphere struct {
-	Center vec.Vec3
-	Radius float64
-	Col    color.Color
+	Center         vec.Vec3
+	Radius         float64
+	Col            color.RGBA
+	EasingDistance float64
 }
 
 func (s Sphere) Intersects(ray Ray) (bool, vec.Vec3, vec.Vec3, float64, float64) {
@@ -132,21 +165,32 @@ func (s Sphere) Intersects(ray Ray) (bool, vec.Vec3, vec.Vec3, float64, float64)
 	// if the origin is inside the sphere of light, it counts as a hit
 	// the results aren't that useful
 	if l2oc < srsq {
-		t0 := 0
-		t1 := 0
+		t0 := float64(0)
+		t1 := float64(0)
 		hit := ray.Origin
-		n := -1 * oc
-		return true, hit, n, t0, t1
+		oc.Multiply(-1)
+		return true, hit, oc, t0, t1
 	}
 
 	thc := math.Sqrt(t2hc)
 	t0 := t_ca - thc
 	t1 := t_ca + thc
 
-	dist := easing_distance * t0
-	//hit := Add(ray.Origin, (
+	dist := s.EasingDistance * t0
+	rd.Multiply(dist)
+	hit := vec.Add(ray.Origin, rd)
 
-	return false, *vec.MakeVec3(0, 0, 0), *vec.MakeVec3(0, 0, 0), 0, 0
+	n := vec.Subtract(hit, sc)
+	n.Divide(s.Radius)
+
+	/*
+		fmt.Println(hit)
+		fmt.Println(n)
+		fmt.Println(t0)
+		fmt.Println(t1)
+	*/
+
+	return true, hit, n, t0, t1
 }
 
 func main() {
